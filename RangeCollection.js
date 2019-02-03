42f61dcd3c58d2d155cba6f2eb48c27f65a320bf
Anton Karpenko <karpolan@karpolan.com>
2019-02-03 20:14:26 +0200

    intersectRanges() logic added

--- RangeCollection.js
+++ RangeCollection.js
@@ -2,6 +2,48 @@
 // A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
 // A range collection is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
 
+
+/**
+ * Returns the array of range intersection. Moved outside the RangeCollection class as a generic utility. 
+ * @param {Array<number>} rangeInclude - [a1, a2] two integers that specify beginning and end of include range.
+ * @param {Array<number>} rangeExclude - [b1, b2] two integers that specify beginning and end of exclude range.
+ * @returns
+ *   []               - fully excluded
+ *   [a1, a2]         - no intersection
+ *   [a1, b1, b2, a2] - splited
+ *   [a1, b1]         - end is cutted
+ *   [b2, a2]         - begin is cutted
+ */
+const intersectRanges = (rangeInclude, rangeExclude) => {
+  if (rangeInclude === []) return []; // empty range
+  if (rangeExclude === []) return rangeInclude; // nothing to exlude
+
+  if (rangeInclude.length > 2) { 
+    // Recursion is needed
+    // Todo: Make a tail recursion or skip first 2 elements in rangeInclude and combine the result with normal flow
+    const result = [];
+    for (let i = 0; i < rangeInclude.length; i += 2)
+      result.push(intersectRanges([rangeInclude[i], rangeInclude[i+1]], rangeExclude))
+    return result; 
+  }
+
+  // We need range constrains in ASC order
+  const [a1, a2] = rangeInclude.concat().sort((a, b) => a - b); 
+  const [b1, b2] = rangeExclude.concat().sort((a, b) => a - b); 
+  console.log('include: [%s, %s], exclude: [%s, %s]', a1, a2, b1, b2);
+
+  if ((b1 <= a1) && (a2 <= b2)) return []; // fully excluded
+  if ((a2 < b1) || (a1 > b2)) return [a1, a2]; // no intersection
+  if ((a1 < b1) && (b2 < a2)) return [a1, b1, b2, a2] ; // splited
+  if ((b1 <= a2) && (a2 <= b2)) return [a1, b1]; // end is cutted
+  if ((b1 <= a1) && (a1 <= b2)) return [b2, a2]; // begin is cutted
+
+  // WTF?
+  console.error('intersectRanges() failed');
+}
+
+
+
 /**
  * RangeCollection class
  * NOTE: Feel free to add any extra member variables/functions you like.
@@ -15,17 +57,35 @@ class RangeCollection {
     this.rangesInclude = []; 
     this.rangesExclude = []; 
 
-
     this.rangesInclude  = [ [1, 5], [10, 20], [17, 21] ]; // Test
     this.rangesExclude = [ [13, 17] ]; // Test
-}
+  }
 
   /**
    * Returns current set of ranges as flat (single dimension) array
    */
   getFlatRanges () {
+    const ranges = [];
+
+    this.rangesInclude.forEach(rangeInclude => {
+      let newRange = rangeInclude;
+      for (let i = 0; i < this.rangesExclude.length; i++) {
+        newRange = intersectRanges(newRange, this.rangesExclude[i]); // deduct every excluded range
+      }
+      if (newRange.length > 0)
+      ranges.push(newRange); // rangeInclude without any excluded ranges
+    });
+    
+    const result = ranges.reduce((acc, val) => acc.concat(val), []); // In new syntax Array.flat();
+    console.log('getFlatRanges() : ', result);
+    return result;
+
+/*
+    const result = this.rangesInclude.reduce((acc, val) => acc.concat(val), []); // In new syntax Array.flat();
+
     // return this.rangesInclude.flat();
     return this.rangesInclude.reduce((acc, val) => acc.concat(val), []);
+*/    
   }
 
   /**
@@ -51,7 +111,7 @@ class RangeCollection {
     const flatRanges = this.getFlatRanges();
     let output = '';
     for (let i = 0; i < flatRanges.length; i += 2)
-      output += `[${flatRanges[i]}, ${flatRanges[i+1]}) `; // "[a, b)"
+      output += `[${flatRanges[i]}, ${flatRanges[i+1]}) `; // "[a, b) "
     console.log(output);
   }
 }
@@ -62,6 +122,8 @@ const rc = new RangeCollection();
 rc.add([1, 5]);
 rc.print();
 // Should display: [1, 5)
+
+return;
 
 rc.add([10, 20]);
 rc.print();
